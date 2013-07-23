eb8d3d8efc2ccf4ba1beb276c4ee94d5b8a648d5
Justin Miller <jmiller9@gmail.com>
2013-07-23 19:45:18 -0400

    Fixed project to work on AndroidStudio 0.2

--- Quiz/build/source/r/debug/com/tuts/quiz/R.java
+++ Quiz/build/source/r/debug/com/tuts/quiz/R.java
@@ -10,53 +10,72 @@ package com.tuts.quiz;
 public final class R {
     public static final class attr {
     }
+
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        /**
+         * From: file:/C:/Users/Justin/AndroidStudioProjects/AndroidQuiz/Quiz/src/main/res/values/dimens.xml
+         * From: file:/C:/Users/Justin/AndroidStudioProjects/AndroidQuiz/Quiz/src/main/res/values-sw720dp-land/dimens.xml
+         */
+        public static final int activity_horizontal_margin = 0x7f040000;
+        public static final int activity_vertical_margin = 0x7f040001;
     }
+
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher = 0x7f020000;
     }
+
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int false_button=0x7f080002;
-        public static final int next_button=0x7f080003;
-        public static final int question_text_view=0x7f080000;
-        public static final int true_button=0x7f080001;
+        public static final int action_settings = 0x7f080004;
+        public static final int false_button = 0x7f080002;
+        public static final int next_button = 0x7f080003;
+        public static final int question_text_view = 0x7f080000;
+        public static final int true_button = 0x7f080001;
     }
+
     public static final class layout {
-        public static final int activity_quiz=0x7f030000;
+        public static final int activity_quiz = 0x7f030000;
     }
+
     public static final class menu {
-        public static final int quiz=0x7f070000;
+        public static final int quiz = 0x7f070000;
     }
+
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int correct_toast=0x7f050002;
-        public static final int false_button=0x7f050003;
-        public static final int incorrect_toast=0x7f050004;
-        public static final int menu_settings=0x7f050005;
-        public static final int next_button=0x7f050006;
-        public static final int question_africa=0x7f050007;
-        public static final int question_americas=0x7f050008;
-        public static final int question_asia=0x7f050009;
-        public static final int question_mideast=0x7f05000a;
-        public static final int question_oceans=0x7f05000b;
-        public static final int true_button=0x7f05000c;
+        /**
+         * From: file:/C:/Users/Justin/AndroidStudioProjects/AndroidQuiz/Quiz/src/main/res/values/strings.xml
+         */
+        public static final int action_settings = 0x7f050000;
+        public static final int app_name = 0x7f050001;
+        public static final int correct_toast = 0x7f050002;
+        public static final int false_button = 0x7f050003;
+        public static final int incorrect_toast = 0x7f050004;
+        public static final int menu_settings = 0x7f050005;
+        public static final int next_button = 0x7f050006;
+        public static final int question_africa = 0x7f050007;
+        public static final int question_americas = 0x7f050008;
+        public static final int question_asia = 0x7f050009;
+        public static final int question_mideast = 0x7f05000a;
+        public static final int question_oceans = 0x7f05000b;
+        public static final int true_button = 0x7f05000c;
     }
+
     public static final class style {
-        /** 
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
+        /**
+         * From: file:/C:/Users/Justin/AndroidStudioProjects/AndroidQuiz/Quiz/src/main/res/values/styles.xml
+         * <p/>
+         * Theme customizations available in newer API levels can go in
+         * res/values-vXX/styles.xml, while customizations related to
+         * backward-compatibility can go here.
+         * <p/>
+         * From: file:/C:/Users/Justin/AndroidStudioProjects/AndroidQuiz/Quiz/src/main/res/values-v11/styles.xml
+         * API 11 theme customizations can go here.
+         * From: file:/C:/Users/Justin/AndroidStudioProjects/AndroidQuiz/Quiz/src/main/res/values-v14/styles.xml
+         * API 14 theme customizations can go here.
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme = 0x7f060000;
+        /**
+         * All customizations that are NOT specific to a particular API-level can go here.
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme = 0x7f060001;
     }
 }
